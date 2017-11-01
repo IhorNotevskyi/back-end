4fd19adda34d6103414bdd942b65cb2d60f21b0b
IhorNotevskyi <32827966+IhorNotevskyi@users.noreply.github.com>
2017-11-02 01:42:05 +0200

    Add files via upload

---
+++ contact_form(style+button_delete)/views/layouts/main.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @var string $content
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/contact_form-button_delete/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/contact_form-button_delete/vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="/contact_form-button_delete/css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <?= $content ?>
+        </div>
+    </body>
+</html>
