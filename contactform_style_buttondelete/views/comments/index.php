92a3d5412b649e7932951c9b595f309d445fa7e9
IhorNotevskyi <32827966+IhorNotevskyi@users.noreply.github.com>
2017-11-02 02:27:23 +0200

    Add files via upload

---
+++ contactform_style_buttondelete/views/comments/index.php
@@ -0,0 +1,62 @@
+<?php
+
+$comments = [];
+
+$dir = __DIR__ . '/../../data';
+foreach (scandir($dir) as $file) {
+    if (in_array($file, ['.', '..', '.gitignore'])) {
+        continue;
+    }
+
+    $content = file_get_contents("{$dir}/{$file}");
+    $row = json_decode($content, true);
+    $row['file'] = $file;
+    $comments[] = $row;
+}
+
+$currentComment = [];
+$saveAttr = '';
+if (array_key_exists('file', $_GET)) {
+    $currentContent = file_get_contents("{$dir}/{$_GET['file']}");
+    $currentComment = json_decode($currentContent, true);
+    $saveAttr = "?file={$_GET['file']}";
+}
+
+?>
+
+<div class="row">
+    <div class="col-lg-12 form-length">
+        <h1 class="text-center">Contact form</h1>
+        <form method="post" action="/contactform_style_buttondelete/save.php<?= $saveAttr ?>" class="text-center">
+            <div class="form-group">
+                <label class="big-label">Enter your name</label>
+                <input
+                    type="text"
+                    name="username"
+                    value="<?= ($currentComment ? $currentComment['name'] : null) ?>"
+                    class="form-control input-lg">
+            </div>
+            <div class="form-group">
+                <label class="big-label">Enter your comment</label>
+                <textarea name="comment" class="form-control input-lg"><?= ($currentComment ? $currentComment['comment'] : null) ?></textarea>
+            </div>
+
+            <input type="submit" value="Submit" class="btn btn-success position">
+            <a href="/contactform_style_buttondelete" class="btn btn-danger">Cancel</a>
+        </form>
+
+    </div>
+</div>
+
+<?php foreach ($comments as $comment) : ?>
+
+    <div class="row">
+        <div class="col-lg-12 comments">
+            <p class="bg-danger pin"><b>Author:</b> <?= $comment['name'] ?></p>
+            <p class="bg-danger pin"><b>Comment:</b><br> <?= nl2br($comment['comment']) ?></p>
+            <a href="/contactform_style_buttondelete/?file=<?= $comment['file'] ?>" class="btn btn-xs btn-info">Edit</a>
+            <a href="/contactform_style_buttondelete/delete.php?file=<?= $comment['file'] ?>" class="btn btn-xs btn-danger">Delete</a>
+        </div>
+    </div>
+
+<?php endforeach; ?>
---
+++ contactform_style_buttondelete/views/layouts/main.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @var string $content
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="/contactform_style_buttondelete/css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <?= $content ?>
+        </div>
+    </body>
+</html>
