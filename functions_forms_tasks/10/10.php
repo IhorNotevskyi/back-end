0f7e14f3c4c38df2cde1c77896b7e0ef0f7572d0
IhorNotevskyi <32827966+IhorNotevskyi@users.noreply.github.com>
2017-11-09 11:23:09 +0200

    Update 10.php

--- functions_forms_tasks/10/10.php
+++ functions_forms_tasks/10/10.php
@@ -3,7 +3,8 @@
 if(isset($_POST['words']))
 {
     $words = $_POST['words'];
-    function uniqueWords($text) {
+    function uniqueWords($text) 
+    {
         $array = explode(" ", mb_strtolower($text));
         $array = array_count_values($array);
         $array = array_count_values($array);
