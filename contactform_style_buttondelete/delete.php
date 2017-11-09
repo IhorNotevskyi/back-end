3aab7d3ab46f20030525344dc66193684bb2abff
IhorNotevskyi <32827966+IhorNotevskyi@users.noreply.github.com>
2017-11-09 09:18:40 +0200

    Update delete.php

--- contactform_style_buttondelete/delete.php
+++ contactform_style_buttondelete/delete.php
@@ -1,10 +1,8 @@
 <?php
 
-$fileName = __DIR__ . '/data/' . $_GET['file'];
-
-if (file_exists($fileName)) {
-    if (isset($_GET['file']) && !empty($_GET['file'])) {
-        $fileDelete = __DIR__ . '/data/' . $_GET['file'];
+if (isset($_GET['file']) && !empty($_GET['file'])) {
+    $fileDelete = __DIR__ . '/data/' . $_GET['file'];
+    if (file_exists($fileDelete)) {
         unlink($fileDelete);
         header("Location: /contactform_style_buttondelete", true, 301);
     }
