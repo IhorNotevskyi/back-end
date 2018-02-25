2d685f53c22830e09971334a9205b9b1cae7d6bf
IhorNotevskyi <32827966+IhorNotevskyi@users.noreply.github.com>
2018-02-25 18:22:08 +0200

    Delete create.php

--- 26_09_samples-master/shop/views/categories/create.php
+++
@@ -1,8 +0,0 @@
-<form method="post">
-    <div class="form-group">
-        <label>Category name</label>
-        <input type="text" name="title" class="form-control">
-    </div>
-
-    <input type="submit" value="Save" class="btn btn-success">
-</form>
